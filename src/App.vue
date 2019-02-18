6c3ac869d5cc1177ddda75fa02790fdc0187aa0a
mirrordingjing <mirrordingjing@didichuxing.com>
2019-02-19 00:35:49 +0800

    v1.0.2 query answer with score & bgm control mod

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,6 @@
 export default {
     created() {
         console.log("miniapp created!!!");
-    },
-    mounted() {
-        const backgroundAudioManager = wx.getBackgroundAudioManager();
-        backgroundAudioManager.title = "步步高";
-        backgroundAudioManager.epname = "步步高";
-        backgroundAudioManager.singer = "中央民族乐团";
-        backgroundAudioManager.coverImgUrl =
-            // "http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000";
-            "http://y.gtimg.cn/music/photo_new/T002R800x800M000004DC0NR23DKVD.jpg?max_age=2592000";
-        // 设置了 src 之后会自动播放
-        backgroundAudioManager.src =
-            // 可仿照官方链接拼接
-            // "http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46"; 
-            "http://isure.stream.qqmusic.qq.com/C400001rjId84BMaEK.m4a?guid=9409697232&vkey=B189EC590816937E068AB49DEDF532FC2249330809C3E2082D6E32DBC656EBF747EB8C2958A0F865005BACD33470B2C58056974FC79700A6&uin=0&fromtag=66";
     }
 };
 </script>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="container">
         <!-- <div class="message">{{msg}}</div> -->
+        <div class="bgm-btn">
+            <img class="note-pic" src="http://thyrsi.com/t6/672/1550507455x2890174321.png" @click=revertBgm()>
+        </div>
         <div class="box">
             <img src="/static/images/lantern.png" mode="widthFix">
         </div>
@@ -14,12 +17,13 @@
             </div>
             <!-- <div>
                 <span>{{userInfo.nickName}}</span>
-            </div> -->
+            </div>-->
             <div class="user-bar">猜中{{correctCount}}条</div>
+            <div class="user-bar" style="margin-top:5px; text-align:right;">{{score}}积分</div>
         </div>
         <div class="riddle-bar">
             <div v-if="correct===0&&!showRiddle" @click="getSingleRiddle">点击抽取</div>
-            <div v-else-if="correct===1||correct===2" @click="getSingleRiddle">再抽一条</div>
+            <div v-else-if="correct===1||correct===3" @click="getSingleRiddle">再抽一条</div>
             <div v-else @click="getSingleRiddle">跳过本条</div>
         </div>
         <div class="pop-container" v-if="showRiddle">
@@ -45,7 +49,17 @@
                     <div>答对了!</div>
                     <div>{{explanation}}</div>
                 </div>
-                <div class="scroll" v-else>不对哦~再试试</div>
+                <div class="scroll" v-else-if="correct==2">不对哦~</div>
+                <div v-if="correct==2" @click="reDisplayRiddle" style="padding:5px;">
+                    <span class="refresh">再猜一次</span>
+                </div>
+                <div v-if="correct==0||correct==2">
+                    <span class="query-answer" @click="queryRiddleAnswer">查看答案（10积分）</span>
+                </div>
+                <div v-if="correct==3">
+                    <div>谜底：{{answer}}</div>
+                    <div>{{explanation}}</div>
+                </div>
             </div>
         </div>
     </div>
@@ -68,25 +82,27 @@ export default {
             correctCount: 0,
             openid: wx.getStorageSync("openid"),
             correct: 0,
-            explanation:""
+            explanation: "",
+            score: 0,
+            bgmState: true
         };
     },
 
-    onload(){
+    onload() {
         wx.loadFontFace({
-          family: 'webfont',
-          source: 'url("//at.alicdn.com/t/webfont_mjy7t30ao5.eot")',
-        //   source: 'url("//down.izihun.com/font/font/488/font/font.ttf?e=1548768793&token=VD7rjRLQpyBmrGT1RTx31HKU9_CMCYnZXpSBHHUq:RlOsQ0jz-duHqVn-z3TR62JJdr8=&attname=%E5%AD%97%E9%AD%8224%E5%8F%B7-%E9%95%87%E9%AD%82%E6%89%8B%E4%B9%A6.ttf&v=1548764383")'
-          success: function (res) {
-              console.log(res.status) //  loaded
-          },
-          fail: function (res) {
-              console.log(res.status) //  error
-          },
-          complete: function (res) {
-              console.log(res.status);
-          }
-      });
+            family: "webfont",
+            source: 'url("//at.alicdn.com/t/webfont_mjy7t30ao5.eot")',
+            //   source: 'url("//down.izihun.com/font/font/488/font/font.ttf?e=1548768793&token=VD7rjRLQpyBmrGT1RTx31HKU9_CMCYnZXpSBHHUq:RlOsQ0jz-duHqVn-z3TR62JJdr8=&attname=%E5%AD%97%E9%AD%8224%E5%8F%B7-%E9%95%87%E9%AD%82%E6%89%8B%E4%B9%A6.ttf&v=1548764383")'
+            success: function(res) {
+                console.log(res.status); //  loaded
+            },
+            fail: function(res) {
+                console.log(res.status); //  error
+            },
+            complete: function(res) {
+                console.log(res.status);
+            }
+        });
     },
 
     mounted() {
@@ -96,10 +112,25 @@ export default {
         }
         var _this = this;
         get("/user/correctCount", { openid: this.openid }).then(res => {
-            this.correctCount = res.data;
+            this.correctCount = res.data.count;
+            this.score = res.data.score;
         });
         this.userInfo = wx.getStorageSync("userInfo");
         this.openid = wx.getStorageSync("openid");
+
+        const backgroundAudioManager = wx.getBackgroundAudioManager();
+        backgroundAudioManager.title = "步步高";
+        backgroundAudioManager.epname = "步步高";
+        backgroundAudioManager.singer = "中央民族乐团";
+        backgroundAudioManager.coverImgUrl =
+            // "http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000";
+            "http://y.gtimg.cn/music/photo_new/T002R800x800M000004DC0NR23DKVD.jpg?max_age=2592000";
+        // 设置了 src 之后会自动播放
+        backgroundAudioManager.src =
+            // 可仿照官方链接拼接
+            // "http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46";
+            "http://isure.stream.qqmusic.qq.com/C400001rjId84BMaEK.m4a?guid=9409697232&vkey=B189EC590816937E068AB49DEDF532FC2249330809C3E2082D6E32DBC656EBF747EB8C2958A0F865005BACD33470B2C58056974FC79700A6&uin=0&fromtag=66";
+        // "http://ws.stream.qqmusic.qq.com/C400001rjId84BMaEK.m4a?guid=9409697232&vkey=B189EC590816937E068AB49DEDF532FC2249330809C3E2082D6E32DBC656EBF747EB8C2958A0F865005BACD33470B2C58056974FC79700A6&uin=0&fromtag=66";
     },
 
     methods: {
@@ -109,15 +140,16 @@ export default {
         async getSingleRiddle() {
             var _this = this;
             const openid = wx.getStorageSync("openid");
-            const data = await get("/riddle/single", {openid:openid});
+            const data = await get("/riddle/single", { openid: openid });
             this.riddle = data.data.question;
             this.hint = data.data.hint;
             this.riddleId = data.data.id;
+            this.answer = data.data.answer;
             this.showRiddle = true;
             this.correct = 0;
             this.userAnswer = "";
             // 待删除在确认时再赋值
-            this.explanation=data.data.explanation;
+            this.explanation = data.data.explanation;
             console.log(this.riddle);
         },
         async confirm() {
@@ -125,15 +157,58 @@ export default {
             var userAnswer = this.userAnswer;
             var riddleId = this.riddleId;
             const openid = wx.getStorageSync("openid");
-            const res = await get("/riddle/verify", { answer: userAnswer,riddleId:riddleId,openid:openid });
+            const res = await get("/riddle/verify", {
+                answer: userAnswer,
+                riddleId: riddleId,
+                openid: openid
+            });
             if (res != null && res.data == true) {
                 this.correct = 1;
-                this.correctCount+=1;
+                this.correctCount += 1;
+                this.score += 5;
             } else {
                 this.correct = 2;
             }
             this.userAnswer = "";
             console.log("correct:" + res.data);
+        },
+        reDisplayRiddle() {
+            this.correct = 0;
+            this.userAnswer = "";
+        },
+        queryRiddleAnswer() {
+            if (this.score < 10) {
+                wx.showToast({
+                    title: "积分不够用啦 \r\n 继续加油哦",
+                    icon: "none",
+                    duration: 2000 //持续的时间
+                });
+            } else {
+                var riddleId = this.riddleId;
+                var openid = this.openid;
+                get("/riddle/queryAnswer", {riddleId: riddleId,openid: openid}).then(res => {
+                    if (res.errno === 0) {
+                        this.score -= 10;
+                        this.correct = 3;
+                    } else {
+                        var errmsg = res.errmsg;
+                        wx.showToast({
+                            title: errmsg,
+                            icon: "none",
+                            duration: 2000 //持续的时间
+                        });
+                    }
+                });
+            }
+        },
+        revertBgm(){
+            const backgroundAudioManager = wx.getBackgroundAudioManager();
+            if(this.bgmState){
+                backgroundAudioManager.pause();
+            }else{
+                backgroundAudioManager.play();
+            }
+            this.bgmState=!this.bgmState;
         }
     }
 };
@@ -159,11 +234,11 @@ div.box img {
     text-align: center;
     margin: 0 28px;
     min-height: 20%;
-    border-style:double;
+    border-style: double;
     border-color: #f03232;
     border-radius: 0.5em;
     border-width: 7px;
-    padding:5px;
+    padding: 5px;
 }
 
 .pop-container > div {
@@ -188,13 +263,27 @@ input {
     color: #eee;
 }
 
-.user-bar{
-    color:#eee;
+.user-bar {
+    color: #eee;
     background-color: #f03232;
     border-radius: 0.1em;
     font-weight: lighter;
     margin-right: 5px;
     padding: 2px 5px;
+}
+
+div .bgm-btn{
+    position: absolute;
+    top: 5%;
+    right: 30px;
+    font-size: 14px;
+}
+
+.note-pic {
+    width: 64rpx;
+    height: 64rpx;
+    border:1;
+    border-color:#f03232;
 }
 
 div .userinfo-bar {
@@ -207,17 +296,17 @@ div .userinfo-bar {
 div .riddle-bar {
     position: absolute;
     top: 19%;
-    left:46%;
+    left: 46%;
     right: 46%;
     padding: 6px;
-    border:#f03232;
+    border: #f03232;
     border-radius: 0.5em;
     background-color: #f03232;
-    color:burlywood;
+    color: burlywood;
 }
 
-div .question{
-    color:#f03232;
+div .question {
+    color: #f03232;
 }
 
 /* 头像 */
@@ -229,21 +318,43 @@ div .question{
 }
 
 /* 字体 */
-.web-font{
-    font-family:"webfont" !important;
-    font-size:16px;font-style:normal;
+.web-font {
+    font-family: "webfont" !important;
+    font-size: 16px;
+    font-style: normal;
     -webkit-font-smoothing: antialiased;
     -webkit-text-stroke-width: 0.2px;
     -moz-osx-font-smoothing: grayscale;
-    color:#333;
+    color: #333;
 }
 
-.answer-input{
-    border:1px,solid,#f03232;
+.answer-input {
+    border: 1px, solid, #f03232;
 }
 
-.scroll{
+.scroll {
     margin: 5px 0 0 0;
 }
 
+.refresh {
+    color: #eee;
+    background-color: #f03232;
+    border-radius: 0.5em;
+    font-weight: lighter;
+    margin-right: 5px;
+    padding: 2px 5px;
+    margin: 5px;
+}
+
+.query-answer {
+    color: burlywood;
+    background-color: #f03232;
+    border-width: 1px;
+    border-radius: 0.5em;
+    /* font-weight: lighter; */
+    margin-right: 5px;
+    padding: 2px 5px;
+    margin: 5px;
+    margin-top: 5px;
+}
 </style>
